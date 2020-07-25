a3bb99f8bbdff00e3a2508a63be3dfb9ce036649
github username <github email address>
2020-07-24 21:10:00 -0500

    add to portfolio functionality and stonk img

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ class Header extends Component {
     return (
       <header>
         <h1 className="text-center">Flatiron Stock Exchange</h1>
+        <img className="stonk-img" src="https://i.kym-cdn.com/entries/icons/mobile/000/029/959/Screen_Shot_2019-06-05_at_1.26.32_PM.jpg"/>
       </header>
     );
   }
